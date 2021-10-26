3dcb94328e3b35df37e11924d7fd6073adbc8980
Die5el <krystianp121@gmail.com>
2021-10-26 21:24:08 +0200

    added ourgames site, minor changes

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Homepage - Nitrosoft Games</title>
 </head>
 <body>
-    
+    Strona główna NitroSoft Games
+    <br>
+    <a href="ourgames.php">Our Games</a>
 </body>
 </html>
